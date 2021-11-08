a0241e258c68e34d87fd711e1669ba67baa99a0d
Beyzanur Çoban <68052018+beyzanurcoban@users.noreply.github.com>
2021-11-08 17:12:10 +0300

    dfs added

--- pstraverse.c
+++ pstraverse.c
@@ -6,5 +6,18 @@
 
 // DFS over a process tree
 void dfs(struct task_struct *task) {
- 
+     struct task_struct *next;
+     struct list_head *list;
+	
+	    printk(KERN_INFO "Name: %30s, Process ID: %d\n", task->comm, task->pid);
+	
+	    list_for_each(list, &task->children) {
+		        next = list_entry(list, struct task_struct, sibling);
+		        dfs(next);
+	    }
 }
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Beyza-Kerem");
+MODULE_DESCRIPTION("Process Tree DFS-BFS");
