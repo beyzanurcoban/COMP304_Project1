9f9619186b9f1d08b36dfe230e98c71739a7cb0f
Beyzanur Çoban <68052018+beyzanurcoban@users.noreply.github.com>
2021-11-08 16:58:54 +0300

    Create pstraverse.c

---
+++ pstraverse.c
@@ -0,0 +1,10 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/sched/task.h>
+#include <linux/module.h>
+
+// DFS over a process tree
+void dfs(struct task_struct *task) {
+ 
+}
