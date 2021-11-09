65aaa56ca7b208b9db10999915daca985f7b2750
KeremG07 <69321438+KeremG07@users.noreply.github.com>
2021-11-09 23:45:34 +0300

    system clear

--- shellington.c
+++ shellington.c
@@ -729,7 +729,7 @@ int kerem_awesome_command(struct command_t *command) {
 			printf("\n");
 			int i, j;
 			for(i=0; i<60; i++) {
-				os.system('clear');
+				system("clear");
 				if(i%4==0 || i%4==2) {
 					for(j=0; j<i; j++) printf(" "); printf("                                                   -:++++:.              \n");
 					for(j=0; j<i; j++) printf(" "); printf("                                    /y/         `oo:.   `./oo`           \n");
@@ -809,7 +809,7 @@ int kerem_awesome_command(struct command_t *command) {
 			printf("\n");
 			int i, j;
 			for(i=0; i<30; i++) {
-				os.system('clear');
+				system("clear");
 				if(i%4==0 || i%4==2) {
 					printf("    -+osssssssssssssssssssssssssso+-    \n  -do-`                          `-od-  \n");
 					printf(" `N:"); for(j=0; j<left_space; j++) printf(" ");
